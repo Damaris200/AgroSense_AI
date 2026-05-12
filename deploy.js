1e707faf0e95df2fcd62ef9609330fbd8db40077
Damaris200 <atehdamaris.anyah@ictuniversity.edu.cm>
2026-05-12 12:09:06 +0100

    update for Sonar Qube

---
+++ deploy.js
@@ -0,0 +1,28 @@
+import { NodeSSH } from "node-ssh";
+import path from "path";
+
+const ssh = new NodeSSH();
+
+await ssh.connect({
+  host: "143.198.161.137",
+  username: "root",
+  privateKey: "C:/Users/anyah/.ssh/id_ed25519",
+});
+
+await ssh.putDirectory(path.resolve("."), "/root/AgroSense_AI", {
+  recursive: true,
+  concurrency: 5,
+  validate: (itemPath) => {
+    const base = path.basename(itemPath);
+
+    if (base === "node_modules") return false;
+    if (base === ".git") return false;
+    if (base.startsWith(".")) return false;
+    if (base.endsWith(".log")) return false;
+
+    return true;
+  },
+});
+
+console.log("Upload complete");
+ssh.dispose();
